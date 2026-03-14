889bc97c537844ff84effe62c37cff69706fc6b6
Mo Abualruz <mkh.ruzz@live.com>
2026-03-14 04:59:59 +0100

    chore: add WSL Linux build script and update .gitignore

---
+++ scripts/wsl_build_linux.sh
@@ -0,0 +1,4 @@
+#\!/bin/bash
+export PATH="/home/mkh/flutter/bin:/home/mkh/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+cd /mnt/f/work/crispy-tivi
+flutter build linux --release
